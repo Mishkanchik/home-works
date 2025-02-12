df2570ea9fa6a6b87e35d3a0519a555bbbd22ed4
mishkanchik <mishkanchik594@gmail.com>
2025-02-12 21:53:02 +0200

    Add project files.

---
+++ pd311_mvc_aspnet/Controllers/ProductController.cs
@@ -0,0 +1,113 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using pd311_mvc_aspnet.Data;
+using pd311_mvc_aspnet.Models;
+using System;
+using System.IO;
+using System.Linq;
+using Microsoft.AspNetCore.Http;
+using Microsoft.EntityFrameworkCore;
+
+namespace pd311_mvc_aspnet.Controllers
+{
+    public class ProductController : Controller
+    {
+        private readonly AppDbContext _context;
+
+        public ProductController(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        public IActionResult Index()
+        {
+            var products = _context.Products.Include(p => p.Category).ToList();
+            return View(products);
+        }
+
+        public IActionResult Create()
+        {
+            ViewData["Categories"] = new SelectList(_context.Categories, "Id", "Name");
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Create(Product model, IFormFile ImageFile)
+        {
+            ViewData["Categories"] = new SelectList(_context.Categories, "Id", "Name", model.CategoryId);
+
+            if (!ModelState.IsValid)
+            {
+                return View(model);
+            }
+
+            if (ImageFile != null && ImageFile.Length > 0)
+            {
+                string uploadsFolder = Path.Combine(Directory.GetCurrentDirectory(), "wwwroot/images");
+                string uniqueFileName = Guid.NewGuid().ToString() + "_" + ImageFile.FileName;
+                string filePath = Path.Combine(uploadsFolder, uniqueFileName);
+
+                using (var fileStream = new FileStream(filePath, FileMode.Create))
+                {
+                    ImageFile.CopyTo(fileStream);
+                }
+
+                model.Image = "/images/" + uniqueFileName;
+            }
+
+            model.Id = Guid.NewGuid().ToString();
+            _context.Products.Add(model);
+            _context.SaveChanges();
+
+
+            return RedirectToAction("Index");
+        }
+
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Delete(string id)
+        {
+            var product = _context.Products.FirstOrDefault(p => p.Id == id);
+            if (product == null)
+            {
+                return NotFound();
+            }
+
+            _context.Products.Remove(product);
+            _context.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+
+
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+            public IActionResult Edit(Product model)
+            {
+                if (!ModelState.IsValid)
+                {
+                    ViewData["Categories"] = _context.Categories.ToList();
+                    return View(model);
+                }
+
+                var existingProduct = _context.Products.FirstOrDefault(p => p.Id == model.Id);
+                if (existingProduct == null)
+                {
+                    return NotFound();
+                }
+
+                existingProduct.Name = model.Name;
+                existingProduct.Description = model.Description;
+                existingProduct.Price = model.Price;
+                existingProduct.Amount = model.Amount;
+                existingProduct.CategoryId = model.CategoryId;
+
+                _context.SaveChanges();
+                return RedirectToAction("Index");
+            }
+        }
+    }
+
